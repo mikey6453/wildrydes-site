a93145f3e1ecb9b1426d3de536b0d5e55c95888b
Michael <tvq3bn@virginia.edu>
2023-12-25 01:57:20 -0500

    update aws cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_hYSdnef0i', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44mcfcav8kriuj3jaqut1li3v7',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
